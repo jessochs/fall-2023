2601e82b6a3c77f21f503de1752cf35dca1c8765
jessochs <fac17002@byui.edu>
2023-11-14 19:52:10 -0700

    prophets

--- lesson05/scripts/prophets.js
+++ lesson05/scripts/prophets.js
@@ -7,9 +7,13 @@ const cards = document.querySelector("#cards");
     prophets.forEach((prophet) => {
         let card = document.createElement('section');
         let fullName = document.createElement('h2');
+        let bday = document.createElement('p');
+        let birthPlace = document.createElement('p');
         let portrait = document.createElement('img');
 
-        fullName.textContent = `${prophet.name} ${prophet.lastname}`
+        fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+        bday.textContent = `Date of Birth: ${prophet.birthdate}`;
+        birthPlace.textContent = `Place of birth: ${prophet.birthplace}`
         portrait.setAttribute('src', prophet.imageurl);
         portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
         portrait.setAttribute('loading', 'lazy');
@@ -17,6 +21,8 @@ const cards = document.querySelector("#cards");
         portrait.setAttribute('height', '440');
 
         card.appendChild(fullName);
+        card.append(bday);
+        card.append(birthPlace);
         card.appendChild(portrait);
 
         cards.appendChild(card);
