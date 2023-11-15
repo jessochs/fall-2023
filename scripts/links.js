9986b5f323ced2b04dc27f6901936ff4370a23a2
jessochs <fac17002@byui.edu>
2023-11-14 18:12:51 -0700

    lessons

--- scripts/links.js
+++ scripts/links.js
@@ -1,34 +1,40 @@
 const url = 'https://jessochs.github.io/wdd230/';
-const urlLink = "https://jessochs.github.io/wdd230/data/links.json"
-
-const displayLinks = (weeks) => {
-    const rows = document.querySelector('div.cards');
-
-    weeks.forEach((week) => {
-        let row = document.createElement('ul');
-        let line = document.createElement('li');
-        let lesson = document.createElement('a');
-        let link = document.createElement('a');
-
-        lesson.textContent = `${week.lesson}`;
-        link.setAttribute('href', week.url);
-        link.setAttribute('a', week.title);
-
-        row.appendChild(link);
-        row.appendChild(lesson);
-        row.appendChild(line);
-        rows.appendChild(row);
-
-
-    });
-}
-
+const urlLink = "https://jessochs.github.io/wdd230/data/links.json";
+const lessonData = document.querySelector("#lessons")
 
 async function getLinks() {
     const response = await fetch(urlLink);
     const data = await response.json();
     // console.log(data);
-    displayLinks(data);
+    displayLinks(data.lessons);
   }
-  
+
+
+const displayLinks = (weeks) => {
+    weeks.forEach((week) => {
+        
+        let li = document.createElement('li');
+       
+        week.links.forEach((link) => {
+            let lesson = document.createElement('a');
+            let divider = document.createElement('span');
+            divider.textContent = "|";
+
+            lesson.textContent = link.title;
+            lesson.setAttribute('href', link.url);
+
+            li.appendChild(lesson);
+
+            if (week.links[week.links.length - 1]!== link)
+                li.appendChild(divider);
+
+        });
+
+        lessonData.appendChild(li);
+        
+
+
+    });
+}
+
   getLinks();
