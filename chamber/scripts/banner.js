26c48a21070d06af8bd04e07c87527232daf6b87
jessochs <fac17002@byui.edu>
2023-11-25 17:22:15 -0700

    working

--- chamber/scripts/banner.js
+++ chamber/scripts/banner.js
@@ -7,20 +7,22 @@ if (weekday >=1 && weekday <= 3) {
 	
 	message = " 🤝🏼 Please join us for the Chamber Meet and Greet on Wednesday at 7:00 pm."
 	document.querySelector("#message").textContent = message;
-} 
- 
-let closeButton = document.createElement("button");
-closeButton.textContent = "X";
-closeButton.classList.add("close-button");
 
-closeButton.addEventListener('click', function() {
+	let closeButton = document.createElement("button");
+	closeButton.textContent = "X";
+	closeButton.classList.add("close-button");
+
+	closeButton.addEventListener('click', function() {
 	let removeBanner = document.querySelector("#remove-banner");
 	removeBanner.remove();
 	bannerElement.style.display = 'none'
-});
+	});
 
 let bannerElement = document.querySelector("#banner");
 bannerElement.appendChild(closeButton);
+} 
+ 
+
 
 
 
