ef1663d3f772c570aa7916f9f04a74ce82752733
jessochs <fac17002@byui.edu>
2023-12-07 21:03:57 -0700

    got the table!!

--- final/scripts/rental.js
+++ final/scripts/rental.js
@@ -3,7 +3,64 @@ const tables = document.querySelector('#display');
 
 const displayRentals = (rentals) => {
     let table = document.createElement('table');
-    // let theHead = document.createElement('thead');
+    let theHead = document.createElement('thead');
+    let headSection = document.createElement('tr');
+    let title = document.createElement('th')
+    let rowTitle = document.createElement('tr');
+    let gap = document.createElement('th')
+    let reserve = document.createElement('th');
+    let walkin = document.createElement('th');
+
+    let special = document.createElement('tr');
+    let rentalType = document.createElement('th');
+    let maxp = document.createElement('th');
+    let resHalf = document.createElement('th');
+    let resFull = document.createElement('th');
+    let walkHalf = document.createElement('th');
+    let walkFull = document.createElement('th');
+
+    
+
+    title.textContent = 'Max Pricing';
+    title.setAttribute('colspan', '6')
+
+    gap.textContent = '';
+    gap.setAttribute('colspan', '2')
+    
+    reserve.textContent = 'Reservation';
+    reserve.setAttribute('colspan', '2');
+    walkin.textContent = 'Walk-In'
+    walkin.setAttribute('colspan', '2');
+
+    rentalType.textContent = 'Rental Type';
+    maxp.textContent = 'Max. Persons';
+    resHalf.textContent = 'Half Day (3 hrs)';
+    resFull.textContent = 'Full Day';
+    walkHalf.textContent = 'Half Day (3 hrs)';
+    walkFull.textContent = 'Full Day';
+
+    headSection.appendChild(title);
+    theHead.appendChild(headSection);
+    // table.appendChild(theHead);
+
+    rowTitle.appendChild(gap);
+    rowTitle.appendChild(reserve);
+    rowTitle.appendChild(walkin);
+    theHead.appendChild(rowTitle);
+
+    special.appendChild(rentalType);
+    special.appendChild(maxp);
+    special.appendChild(resHalf);
+    special.appendChild(resFull);
+    special.appendChild(walkHalf);
+    special.appendChild(walkFull);
+    theHead.appendChild(special);
+
+    table.appendChild(theHead);
+
+    let body = document.createElement('tbody');
+    
+    
     rentals.forEach((rental) => {
         let row = document.createElement('tr');
         let type = document.createElement('td');
@@ -27,7 +84,9 @@ const displayRentals = (rentals) => {
         row.appendChild(walk1);
         row.appendChild(walk2);
 
-        table.appendChild(row);
+        body.appendChild(row);
+        table.appendChild(body);
+        tables.appendChild(table);
 
 
     });
