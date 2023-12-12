55df5f0266959a44cd3ca7e143c67e1e682692a0
jessochs <fac17002@byui.edu>
2023-12-12 13:28:16 -0700

    more changes

--- final/scripts/hamburger.js
+++ final/scripts/hamburger.js
@@ -3,6 +3,7 @@ const navigation = document.querySelector('.navigation');
 
 hamButton.addEventListener('click', () => {
     navigation.classList.toggle('open');
-    hamButton.classList.toggle('open');
+    hamButton.classList.toggle('open')
+    
     
 });
--- final/scripts/weather.js
+++ final/scripts/weather.js
@@ -1,51 +1,16 @@
-// const temp = document.querySelector('#temp');
-// const weatherIcon = document.querySelector('#weath-icon');
-// const caption = document.querySelector('figcaption');
-// const humidity = document.querySelector('#humid');
-
-// const finalUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=20.51&lon=-86.94&units=imperial&appid=ff1561849be172f6f7573c3546d94809';
-
-// async function apiFetch() {
-//     try {
-//         const response = await fetch(finalUrl);
-//         if (response.ok) {
-//             const data = await response.json();
-//             // console.log(data);
-//             displayResults(data);
-
-//         }
-//         else {
-//             throw Error (await response.text());
-//         }
-        
-//     } catch (error) {
-//         console.log(error)
-//     }
-// }
-
-// function displayResults(data) {
-//     temp.innerHTML = `${data.main.temp.toFixed(0)} &deg;F`;
-//     const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
-//     let desc = data.weather[0].description;
-//     weatherIcon.setAttribute('src', iconsrc);
-//     weatherIcon.setAttribute('alt', desc);
-//     caption.textContent = `${desc}`;
-//     humidity.innerHTML = `${data.main.humidity}`;
-
-// }
-// apiFetch();
-
 const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
-const dayTemp = document.querySelector('#day-temp');
 
-const chamberUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=20.51&lon=-86.94&units=imperial&appid=ff1561849be172f6f7573c3546d94809';
-const dailyUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=33.54&lon=-112.18&cnt=3&units=imperial&appid=ff1561849be172f6f7573c3546d94809'
+const humidity = document.querySelector('#humid');
+const tempMax = document.querySelector('#max-temp');
+
+const finalUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=20.51&lon=-86.94&units=imperial&appid=ff1561849be172f6f7573c3546d94809';
+const tomorrowUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=20.51&lon=-86.94&cnt=1&appid=ff1561849be172f6f7573c3546d94809'
 
 async function apiFetch() {
     try {
-        const response = await fetch(chamberUrl);
+        const response = await fetch(finalUrl);
         if (response.ok) {
             const data = await response.json();
             // console.log(data);
@@ -62,24 +27,7 @@ async function apiFetch() {
     
 }  
 
-// async function apiFetchDaily() {
-//     try {
-//         const response = await fetch(dailyUrl);
-//         if (response.ok) {
-//             const dataDaily = await response.json();
-//             // console.log(data);
-//             displayResults(dataDaily);
 
-//         }
-//         else {
-//             throw Error (await response.text());
-//         }
-        
-//     } catch (error) {
-//         console.log(error)
-//     }
-    
-// }  
 
 function displayResults(data) {
     currentTemp.innerHTML = `${data.main.temp.toFixed(0)} &deg;F`;
@@ -88,17 +36,119 @@ function displayResults(data) {
     weatherIcon.setAttribute('src', iconsrc);
     weatherIcon.setAttribute('alt', desc);
     captionDesc.textContent = `${desc}`;
+    humidity.innerHTML = `${data.main.humidity} &deg;F`;
+    tempMax.innerHTML = `${data.main.temp_max.toFixed(0)} &deg;F`;
+
+    
+    
+}
+
+
+apiFetch();
+let hoy = new Date();
+let semana = hoy.getDay()
+let message;
+
+if (semana >=1 && semana <= 3) {
+	
+    message = 'The maximum temperature for the day is: ';
+    document.querySelector('#message').textContent = message; 
+
+    let closeButton = document.createElement("button");
+    closeButton.textContent = "X";
+    closeButton.classList.add("close-button");
+
+    closeButton.addEventListener('click', function() {
+    let removeBanner = document.querySelector("#remove-banner");
+    removeBanner.remove();
+    bannerElement.style.display = 'none'
+    });
+
+let bannerElement = document.querySelector("#banner");
+bannerElement.appendChild(closeButton);
+}
+
+// next day forecast
+
+
+
+async function tomorrowFetch() {
+    try {
+        const response = await fetch(tomorrowUrl);
+        if (response.ok) {
+            const dataTomorrow = await response.json();
+            // console.log(data);
+            displayNextDay(dataTomorrow);
+
+        }
+        else {
+            throw Error (await response.text());
+        }
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+tomorrowFetch();
+
+function displayNextDay(dataTomorrow) {
+    const tomorrowList = dataTomorrow.list;
+    const showNextDay = document.getElementById('next-container');
+
+    const dailyTemps = {};
+
+    const todayIndex = new Date().getDate();
+
+    tomorrowList.forEach((dailyValue) => {
+        const timestamp = dailyValue.dt;
+        const date = new Date(timestamp * 1000);
+        const dayIndex = date.getDay();
+
+        if (dayIndex === todayIndex ) {
+            return;
+        }
+
+        const day = date.toLocaleDateString('en-Us', {weekday: 'long'});
+
+        if (!dailyTemps[day]) {
+            dailyTemps[day] = {
+                temperature: dailyValue.main.temp,
+                icon: dailyValue.weather[0].icon,
+                description: dailyValue.weather[0].description,            
+            };
+        }
+    });
+
+    const temperatures = dailyTemps[day];
+    const forecastDay = document.createElement('div');
+    forecastDay.classList.add('forecast-day') 
+
+    const iconsrc = 'https://openweathermap.org/img/w/01d.png';
+    let forecastName = document.createElement('h4');
+    forecastName.textContent = `${day}:`;
+    let temps = document.createElement('p');
+    temps.textContent = `${temperatures.temp.toFixed(0)} &deg;F`;
+
+    let forecastDesc = document.createElement('p');
+    forecastDesc.textContent = temperatures.description;
+
+    let forecastIcon = document.createElement('img');
+    forecastIcon.setAttribute('src', iconsrc);
+    forecastIcon.setAttribute('alt', 'icon image')
+
+    forecastDay.append(forecastName);
+    forecastDay.append(temps);
+    forecastDay.append(forecastIcon);
+    forecastDay.append(forecastDesc);
+
+    showNextDay.appendChild(forecastDay);
+
 
 }
 
-// function displayDailyForcast(dataDaily) {
-    
-//     dayTemp.innerHTML = `${dataDaily.list.temp.day.toFixed(0)} &deg;F`;
-//     let dIcon = `https://openweathermap.org/img/w/${dataDaily.list.weather[0].icon}.png`;
-//     let summ = dataDaily.list.weather[0].description;
-//     dailyIcon.setAttribute('src',dIcon);
-//     dailyIcon.setAttribute('alt',summ );
-//     dailyCaption.textContent = `${summ}`;
-// }
 
-apiFetch();
+
+// Banner button
+
+
